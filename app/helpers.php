488dd8f6f1249e3ea8135acf2643eb630d6888b1
Chhun Piseth <yazugaming18@gmail.com>
2021-10-10 11:38:28 +0700

    Helper function

---
+++ app/helpers.php
@@ -0,0 +1,44 @@
+<?php
+
+function workdays($start,$end,$holidays){
+    $end = strtotime($end);
+    $start = strtotime($start);
+    $days = ($end - $start) / 86400 + 1;
+
+    $no_full_weeks = floor($days / 7);
+    $no_remaining_days = fmod($days, 7);
+
+    $the_first_day_of_week = date("N", $start);
+    $the_last_day_of_week = date("N", $end);
+
+    if ($the_first_day_of_week <= $the_last_day_of_week) {
+        if ($the_first_day_of_week <= 6 && 6 <= $the_last_day_of_week) $no_remaining_days--;
+        if ($the_first_day_of_week <= 7 && 7 <= $the_last_day_of_week) $no_remaining_days--;
+    }
+    else {
+        if ($the_first_day_of_week == 7) {
+            $no_remaining_days--;
+
+            if ($the_last_day_of_week == 6) {
+                $no_remaining_days--;
+            }
+        }
+        else {
+            $no_remaining_days -= 2;
+        }
+    }
+
+$workingDays = $no_full_weeks * 5;
+    if ($no_remaining_days > 0 )
+    {
+    $workingDays += $no_remaining_days;
+    }
+
+    foreach($holidays as $holiday){
+        $time_stamp=strtotime($holiday);
+        if ($start <= $time_stamp && $time_stamp <= $end && date("N",$time_stamp) != 6 && date("N",$time_stamp) != 7)
+            $workingDays--;
+    }
+
+    return $workingDays;
+}
